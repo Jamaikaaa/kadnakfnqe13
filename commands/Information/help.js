26bf53d1d970efdea5a26c50fbe76de5b2751570
Jamaikaaa <92013298+Jamaikaaa@users.noreply.github.com>
2021-10-06 09:38:49 +0300

    Groot Reborn Initial

---
+++ commands/Information/help.js
@@ -0,0 +1,35 @@
+const { MessageEmbed, Message } = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+module.exports = {
+    name: "help",
+    category: "Information",
+    aliases: ["h", "halp"],
+    cooldown: 4,
+    usage: "help",
+    description: "Shows you Help for Milrato x Rythm",
+    run: async (client, message, args, user, text, prefix) => {
+      try{
+        let string = `:white_check_mark: Type \`${prefix}commands\` for a list of Commands\n\n:question: New to **Groot Reborn**? yla 7l 3n 2erna`
+        let embed = new MessageEmbed()
+        .setTitle("Milrato x Rythm Help")
+        .setURL("https://milrato.eu")
+        .setDescription(string)
+        if(message.guild.me.permissionsIn(message.channel).has("EMBED_LINKS")){
+          message.reply(embed)
+        }else{
+          message.reply(string)
+        }
+    } catch (e) {
+        console.log(String(e.stack).bgRed)
+        return message.channel.send(new MessageEmbed()
+            .setColor(ee.wrongcolor)
+            .setFooter(ee.footertext, ee.footericon)
+            .setTitle(`❌ ERROR | An error occurred`)
+            .setDescription(`\`\`\`${e.stack}\`\`\``)
+        );
+    }
+  }
+}
+/**
+*/
